e21a9550f2d1441f697741a7e8ac68e531f63d1c
Monique <moniquesbg@gmail.com>
2023-03-17 09:04:57 +0100

    added student class

---
+++ javafx/src/main/java/com/example/javafx/Student.java
@@ -0,0 +1,58 @@
+package com.example.javafx;
+
+public class Student {
+
+    public String firstName;
+    public String lastName;
+    public int studentNumber;
+
+    public int gpa;
+
+    public Student(String firstName, String lastName, int studentNumber, int gpa)
+    {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.studentNumber = studentNumber;
+        this.gpa = gpa;
+    }
+
+    public String getFirstName()
+    {
+        return this.firstName;
+    }
+
+    public void setFirstName(String firstName)
+    {
+        this.firstName = firstName;
+    }
+
+    public String getLastName()
+    {
+        return this.lastName;
+    }
+
+    public void setLastName(String lastName)
+    {
+        this.firstName = lastName;
+    }
+
+    public int getStudentNumber()
+    {
+        return this.studentNumber;
+    }
+
+    public void setStudentNumber(int studentNumber)
+    {
+        this.studentNumber = studentNumber;
+    }
+
+    public int getGpa()
+    {
+        return this.gpa;
+    }
+
+    public void setGpa(int gpa)
+    {
+        this.gpa = gpa;
+    }
+}
