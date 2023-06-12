eb2c02cae12798653883337e95232549b45436fd
Jessev3 <jesse.vugteveen@student.nhlstenden.com>
2023-06-12 21:02:17 +0200

    added customqueue methods and updated UI

--- javafx/src/main/java/com/example/javafx/MainApplication.java
+++ javafx/src/main/java/com/example/javafx/MainApplication.java
@@ -1,18 +1,10 @@
 package com.example.javafx;
 
-import com.example.javafx.dataset.Student;
-import com.example.javafx.datastructures.LinkedList.CustomLinkedList;
-import com.example.javafx.datastructures.Queue.CustomQueue;
-import com.example.javafx.datastructures.Stack.CustomStack;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
-
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Iterator;
-import java.util.List;
 
 public class MainApplication extends Application {
     @Override
@@ -25,14 +17,6 @@ public class MainApplication extends Application {
     }
 
     public static void main(String[] args) {
-
-        // Binary search tree
-        CustomQueue<Student> queue = new CustomQueue<>();
-        queue.enqueue(new Student("dfsd", "Dfd", 39));
-        queue.enqueue(new Student("dfsd", "Dfd", 40));
-        queue.enqueue(new Student("dfsd", "Dfd", 41));
-        queue.print();
-
         launch();
     }
 }
--- javafx/src/main/java/com/example/javafx/datastructures/Queue/CustomQueue.java
+++ javafx/src/main/java/com/example/javafx/datastructures/Queue/CustomQueue.java
@@ -1,7 +1,9 @@
 package com.example.javafx.datastructures.Queue;
 
-import com.example.javafx.Node;
+import com.example.javafx.dataset.Data;
 import com.example.javafx.dataset.Student;
+
+import java.util.ArrayList;
 
 public class CustomQueue<T extends Student> {
 
@@ -18,10 +20,19 @@ public class CustomQueue<T extends Student> {
     private Node<T> first;
     private Node<T> last;
 
+    private ArrayList<T> students;
+
     public CustomQueue() {
         this.size = 0;
         this.first = null;
         this.last = null;
+        this.students = Data.createDataSet(5);
+    }
+
+    public void buildStack() {
+        for (T s : this.students) {
+            this.enqueue(s);
+        }
     }
 
     public int size() {
@@ -70,15 +81,115 @@ public class CustomQueue<T extends Student> {
         return this.first.value;
     }
 
-    public void print() {
+    public String print() {
         if (isEmpty()) {
-            return;
+            return "";
         }
+
+        StringBuilder studentData = new StringBuilder();
 
         Node<T> node = this.first;
         while (node != null) {
-            System.out.println(node.value);
+            T student = node.value;
+            studentData.append("<First name: " + student.getFirstName() + ", Last Name: " + student.getLastName() + ", Student Number: " + student.getStudentNumber() + "> ");
+            studentData.append("\n");
             node = node.next;
         }
+        return studentData.toString();
+    }
+
+    public boolean search(T studentData)
+    {
+        Node<T> currentNode = this.first;
+
+        while(currentNode != null) {
+
+            Integer studentNumber = currentNode.value.getStudentNumber();
+            String firstName = currentNode.value.getFirstName();
+            String lastName = currentNode.value.getLastName();
+
+            if (studentNumber.compareTo(studentData.getStudentNumber()) == 0
+                    || firstName.compareTo(studentData.getFirstName()) == 0
+                    || lastName.compareTo(studentData.getLastName()) == 0) {
+                return true;
+            }
+
+            currentNode = currentNode.next;
+        }
+        return false;
+    }
+
+    public void sort(String type) {
+        if (this.first == null || this.first.next == null) {
+            return;
+        }
+
+        boolean swapped = true;
+
+        while (swapped) {
+            swapped = false;
+
+            if (type.equals("studentnumber")) {
+                swapped = this.bubbleSort(swapped, "studentnumber");
+            } else if (type.equals("firstname")) {
+                swapped = this.bubbleSort(swapped, "firstname");
+            } else if (type.equals("lastname")) {
+                swapped = this.bubbleSort(swapped, "lastname");
+            }
+        }
+    }
+
+    /**
+     * bubble sort
+     *
+     * @param swapped true or false if swapped
+     * @param type    type of sorting
+     * @return return true or false and sorts list
+     */
+
+    private Boolean bubbleSort(Boolean swapped, String type) {
+        Node<T> prev = null;
+        Node<T> curr = this.first;
+        Node<T> next = curr.next;
+
+        while (next != null) {   //checks if next node exists
+            if (getComparison(type, curr)) { //comparing current with next node
+                swapped = true;
+
+                if (prev != null) {
+                    prev.next = next;
+                } else {
+                    this.first = next;
+                }
+
+                curr.next = next.next;
+                next.next = curr;
+
+                //swap
+                Node<T> temp = curr;
+                curr = next;
+                next = temp;
+            }
+
+            prev = curr;
+            curr = curr.next;
+            next = next.next;
+        }
+        return swapped;
+    }
+
+    /**
+     * get comparison string
+     *
+     * @return true or false
+     */
+    public Boolean getComparison(String type, Node<T> curr) {
+        boolean result = false;
+
+        result = type.equalsIgnoreCase("studentnumber") && curr.value.getStudentNumber() > curr.next.value.getStudentNumber();
+        result = type.equalsIgnoreCase("firstname") ? curr.value.getFirstName().compareTo(curr.next.value.getFirstName()) > 0 : result;
+        result = type.equalsIgnoreCase("lastname") ? curr.value.getLastName().compareTo(curr.next.value.getLastName()) > 0 : result;
+
+        return result;
     }
 }
